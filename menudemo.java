d17831f7612332cef00cc7776bb95e84582d99de
soumyap645 <46665916+soumyap645@users.noreply.github.com>
2019-01-21 12:39:10 +0530

    Add files via upload
    menuswingdemo

---
+++ menudemo.java
@@ -0,0 +1,31 @@
+package swingdemo;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class menudemo extends JFrame{
+	menudemo()
+	{
+		JMenuBar menubar=new JMenuBar();
+		JMenu menu=new JMenu("File");
+		JMenuItem menuitem1=new JMenuItem("new");
+		JMenuItem menuitem2=new JMenuItem("open");
+		menu.add(menuitem1);
+		menu.add(menuitem2);
+		menubar.add(menu);
+		
+		setJMenuBar(menubar);
+		setSize(400, 400);
+		setTitle("MenuDemo");
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new menudemo();
+	}
+
+}
